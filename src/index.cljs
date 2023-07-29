d1fd365a2e59ccd0c698a1b48953529dc14914cf
Sergey Shvets <shvets.serg@gmail.com>
2023-07-29 13:20:26 -0700

    Added scripts and expanded test

--- src/index.cljs
+++ src/index.cljs
@@ -9,11 +9,34 @@
 
 (js/console.log (template-array ["a" "b"]))
 
-
+#_:clj-kondo/ignore
 (defclass HelloWorld
   (extends lit/LitElement)
 
-  (field name "Username")
+  (field fname "Username")
+
+  (constructor
+   [this]
+   (super)
+   )
+
+  Object
+  (render
+   [this]
+   (js/console.log "render is called" this)
+   ;(js-template lit/html "<h1> Hello " name "!<h1>")
+   (lit/html (template-array [ "<h1> Hello " "!</h1>"]) fname)
+   ))
+
+
+(.define js/customElements "hello-world" HelloWorld)
+
+#_:clj-kondo/ignore
+(defclass HelloWorld2
+  (extends lit/LitElement)
+
+  (field fname "Username 2")
+  (field ^boolean checked false)
 
   (constructor
    [this]
@@ -23,18 +46,51 @@
   (render
    [this]
    (js/console.log "render is called" this)
-   ;(js-template lit/html "<h1> Hello " name "!<h1>")
-   (lit/html (template-array [ "<h1> Hello " "!</h1>"]) name)
-   ))
+  ;;  (js-template lit/html "<div><h1> Hello " fname "!</h1>"
+  ;;               "<input placeholder=\"Enter your name\" @input=" (.-handle-click this) " ?disabled=" (not checked) "/>"
+  ;;               "<label><input type=\"checkbox\" @change=" (.-set-checked this) ">Enable Editing</label>" "</div>")
+   (lit/html (template-array ["<div><h1> Hello " fname "!</h1>"
+                              ;; "<input placeholder=\"Enter your name\" @input=" (.-handle-click this) " ?disabled=" (not checked) "/>"
+                              ;; "<label><input type=\"checkbox\" @change=" (.-set-checked this) ">Enable Editing</label>" "</div>"
+                              ]))
+   )
 
+  (handle-click
+   [this event]
+   (js/console.log "click" this)
+   (js/console.log "event" event)
+   (set! fname (.. event -target -value))
+   nil)
+
+  (set-checked
+   [this event]
+   (js/console.log "Setting checkbox value" event)
+   (set! checked (.. event -target -checked)))
+
+  (connectedCallback
+   [this]
+   ;; Object.getPrototypeOf(Dog.prototype).speak.call(this);
+   (super (connectedCallback))
+   ;;(.connectedCallback (super))
+   ;;(js* "super.connectedCallback()")
+   (js/console.log "Hello from Dom" this)))
+
+
+(set! (.-properties HelloWorld2) #js {"fname" #js {} "checked" #js {}})
+(.define js/customElements "hello-world-2" HelloWorld2)
+
+
+(defn add-component-to-app
+  [comp-str]
+  (let [app       (.getElementById js/document "app")
+        custom-el (.createElement js/document comp-str)]
+    (.appendChild app custom-el)))
 
 (defn start
   []
   (js/console.log "Hello, world!")
-  (.define js/customElements "hello-world" HelloWorld)
-  (let [app       (.getElementById js/document "app")
-        custom-el (.createElement js/document "hello-world")]
-    (.appendChild app custom-el))
+  (add-component-to-app "hello-world")
+  (add-component-to-app "hello-world-2")
   )
 
 (start)
